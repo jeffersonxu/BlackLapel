2ed8dd71412008070324bf520626ace80734ff88
Jefferson Xu <jeffersonshoe@outlook.com>
2018-03-14 21:36:29 -0400

    Hallelujah: Dictionary finalized

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+var arraySort = require('array-sort');
 var request = require('request');
 
 //Initializes "Readline" 
@@ -18,29 +19,51 @@ request('https://blacklapel.com/api/product/category/suits', function (error, re
 	for(var key in data){
 		if(data.hasOwnProperty(key)){
 			var suit = data[key].row[0];
-			
-			if(suit.hasOwnProperty("_mageid") && suit.name != "Suits")
-				dict[key] = suit;
+			var keyWords = data[key].row[4];
+
+			for(var word in keyWords){
+				var index = keyWords[word];
+				
+				if(suit.hasOwnProperty("_mageid") && suit.name != "Suits"){
+					var obj = {
+							name: suit.name,
+							price: suit.price
+						}
+
+					//If the index exists just push
+					if(dict[index])
+						dict[index].push(obj)
+					//If index doesn't exist, create new array and push
+					else{
+						dict[index] = [];  
+						dict[index].push(obj);
+					}
+				}
+			}
 		}	
 	}
 
 	rl.question('What color suit would you like to see or type (all) to see every choice available:\n', (answer) => {
 	  	var counter = 0;
-	  	var all = false;
 
-	 	if(answer.toString().trim() === "all")
-	 		all = true;
+		if(answer == "all"){
+			for(var prop in dict){
+				var suits = dict[prop];
 
-		for(var suit in dict){
-			if(!dict[suit].meta_keyword.includes(answer.toLowerCase()) && !all)
-				continue;
-			
-			console.log("$" + dict[suit].price + " - " + dict[suit].name); 
-			counter++;
+				for(var suit in suits)
+					console.log("$" + suits[suit].price + "\t: " + suits[suit].name);
+			}
 		}
-	 	
-	 	if(counter == 0)
-	 		console.log("Sorry, there were no fields found :(");
+		else{
+			if(dict[answer]){
+				dict[answer].forEach(function(suit) {
+					console.log("$" + suit.price + "\t: " + suit.name);
+					counter++;
+				});
+			}
+			else
+				console.log("Sorry, there were no fields found :(")
+		}
 
 	 	console.log("\n\n" + counter + " items returned");
 	    rl.close();
